dec7e84e0a0046049bccfd10113e79c3c07b0e69
Noms1234 <phakathisiyanda0@gmail.com>
2023-03-18 11:35:05 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #includeĺ<stdio.h>
 /**
  * main - Entry point
- *
+ *
  * Return: Always 0 (Success)
- */
+ */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");

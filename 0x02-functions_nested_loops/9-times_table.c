9a64b0ae45d341d93e106e31d1aca87c0670893e
Noms1234 <phakathisiyanda0@gmail.com>
2023-03-26 09:52:19 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,14 @@
 #iniclude "main.h"
 
 
-	/**
-	 * times_table - prints the 9 times table, starting with 0
-	 */
+/**
+* times_table - prints the 9 times table, starting with 0
+*/
 void times_table(void)
 
 {
 		int i, j, k;
+
 
 
 		for (i = 0; i < 10; i++)
@@ -35,6 +36,6 @@ void times_table(void)
 					_putchar((k % 10) + '0');
 				}
 			}
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
+}

6877686b65d2ca7e85644bd47af3d3c9923af8fb
Noms1234 <phakathisiyanda0@gmail.com>
2023-03-31 10:53:10 -0700

    0. Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
+* positive_or_negative - Checks number is positive negative or zero
 * @i: the number is checked
 * Return: always 0
 */
+
 void positive_or_negative(int i)
 	{
 		if (i < 0)

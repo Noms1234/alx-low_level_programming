1fb64452653b06e5e410a07afe6093ba793e1b30
Noms1234 <phakathisiyanda0@gmail.com>
2023-04-20 20:20:50 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - defining new type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

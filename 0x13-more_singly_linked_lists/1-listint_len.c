5734bac4d6e85c6c019bff4018e0573a3ea9e105
Noms1234 <phakathisiyanda0@gmail.com>
2023-07-04 13:03:13 -0700

    unction that returns the number of elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - function adds new node at the beginning of listint_t list
+ * @h: pointer
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
